08c9dd9d19115724751d98c5fdf0b61f2c430c0e
yuyang <2247295596@qq.com>
2019-05-08 10:55:42 +0800

    Site updated: 2019-05-08 10:55:40

--- js/live2d/autoload.js
+++ js/live2d/autoload.js
@@ -1,11 +1,11 @@
 const live2d_path = "https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget/";
 //const live2d_path = "./";
 
-$("<link>").attr({href: "/waifu.css", rel: "stylesheet", type: "text/css"}).appendTo("head");
+$("<link>").attr({href: "/blog/waifu.css", rel: "stylesheet", type: "text/css"}).appendTo("head");
 //waifu.css的绝对路径
 
 $.ajax({
-	url: "/js/live2d/live2d.min.js",
+	url: "/blog/js/live2d/live2d.min.js",
 	dataType: "script",
 	cache: true,
 	async: false
@@ -13,7 +13,7 @@ $.ajax({
 //live2d.min.js的绝对路径
 
 $.ajax({
-	url: "/js/live2d/waifu-tips.js",
+	url: "/blog/js/live2d/waifu-tips.js",
 	dataType: "script",
 	cache: true,
 	async: false
@@ -22,7 +22,7 @@ $.ajax({
 
 //初始化看板娘，会自动加载指定目录下的waifu-tips.json
 $(window).on("load", function() {
-	initWidget("/waifu-tips.json", "https://live2d.fghrsh.net/api");
+	initWidget("/blog/waifu-tips.json", "https://live2d.fghrsh.net/api");
 });
 //initWidget第一个参数为waifu-tips.json的绝对路径
 //第二个参数为api地址（无需修改）
